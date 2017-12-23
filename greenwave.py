ec1d7e2730b57187ed1ec9a350bbc9443054619b
David Fiel <github@dfiel.com>
2017-12-23 12:36:11 -0500

    Working code with GreenwaveReality python module

--- greenwave.py
+++ greenwave.py
@@ -1,4 +1,3 @@
-
 """
 Support for Greenwave Reality (TCP Connected) lights.
 
@@ -11,87 +10,50 @@ from homeassistant.components.light import (
     ATTR_BRIGHTNESS, Light, PLATFORM_SCHEMA, SUPPORT_BRIGHTNESS)
 from homeassistant.const import CONF_HOST
 import homeassistant.helpers.config_validation as cv
+
 SUPPORTED_FEATURES = (SUPPORT_BRIGHTNESS)
-REQUIREMENTS = ['xmltodict==0.11.0']
+REQUIREMENTS = ['greenwavereality']
 _LOGGER = logging.getLogger(__name__)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
 })
 
 
-def pull_xml(host):
-    """Pull XML Data from Gateway."""
-    import xmltodict
-    resp = gateway_action(host, 0, 'xml', 0)
-    return xmltodict.parse(resp.content)
-
-
-def number_range(value, left_min, left_max, right_min, right_max):
-    """Map one number range to another."""
-    # Figure out how 'wide' each range is
-    left_span = left_max - left_min
-    right_span = right_max - right_min
-
-    # Convert the left range into a 0-1 range (float)
-    value_scaled = float(value - left_min) / float(left_span)
-
-    # Convert the 0-1 range into a value in the right range.
-    return int(right_min + (value_scaled * right_span))
-
-
-def get_brightness(device):
-    """Get Brightness of Device."""
-    if 'level' in device:
-        level = number_range(int(device['level']), 1, 100, 1, 255)
-        return level
-    else:
-        return 0
-
-
-def is_online(device):
-    """Check Device Status."""
-    return 'offline' not in device
-
-
-def gateway_action(host, did, action, value):
-    """Do Action for TCP Connected Gateway."""
-    import requests
-    if action == 'xml':
-        url = ('http://' + host + '/gwr/gop.php?cmd=GWRBatch&data=<gwrcmds>'
-        '<gwrcmd><gcmd>RoomGetCarousel</gcmd><gdata><gip><version>1'
-        '</version><token>1234567890</token><fields>name,status</fields>'
-        '</gip></gdata></gwrcmd></gwrcmds>&fmt=xml')
-        resp = requests.get(url)
-        return resp
-    if action == 'level':
-        action = '<type>level</type>'
-    else:
-        action = ''
-    url = ('http://' + host + '/gwr/gop.php?cmd=DeviceSendCommand&data=<gip>'
-    '<version>1</version><token>1234567890</token><did>' + did + '</did>'
-    '<value>' + str(value) + '</value>' + action + '</gip>&fmt=xml')
-    requests.get(url)
-
-
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup TCP Connected Platform."""
+    import greenwavereality as greenwave
+    import os
     host = config.get(CONF_HOST)
-    doc = pull_xml(host)
-    add_devices(TcpLights(device, host) for device in
-        doc['gwrcmds']['gwrcmd']['gdata']['gip']['room']['device'])
+    tokenfile = hass.config.path('greenwave.token')
+    if config.get("version") == 3:
+        if os.path.exists(tokenfile):
+            tokenfile = open(tokenfile)
+            token = tokenfile.read()
+            tokenfile.close()
+        else:
+            token = greenwave.grab_token(host, 'hass', 'homeassistant')
+            tokenfile = open(tokenfile, "w+")
+            tokenfile.write(token)
+            tokenfile.close()
+    else:
+        token = None
+    doc = greenwave.grab_xml(host, token)
+    add_devices(TcpLights(device, host, token) for device in doc)
 
 
 class TcpLights(Light):
     """Representation of an TCP Connected Light."""
 
-    def __init__(self, light, host):
+    def __init__(self, light, host, token):
         """Initialize a TCP Connected Light."""
+        import greenwavereality as greenwave
         self._did = light['did']
         self._name = light['name']
         self._state = int(light['state'])
-        self._brightness = get_brightness(light)
+        self._brightness = greenwave.hass_brightness(light)
         self._host = host
-        self._online = is_online(light)
+        self._online = greenwave.check_online(light)
+        self.token = token
 
     @property
     def supported_features(self):
@@ -102,11 +64,6 @@ class TcpLights(Light):
     def available(self):
         """Return True if entity is available."""
         return self._online
-
-    @property
-    def did(self):
-        """Return the device id of this light."""
-        return self._did
 
     @property
     def name(self):
@@ -125,23 +82,24 @@ class TcpLights(Light):
 
     def turn_on(self, **kwargs):
         """Instruct the light to turn on."""
-        temp_brightness = str(number_range(kwargs.get(ATTR_BRIGHTNESS, 255),
-        1, 255, 1, 100))
-        gateway_action(self._host, self._did, 'level', temp_brightness)
-        gateway_action(self._host, self._did, 'on', 1)
+        import greenwavereality as greenwave
+        temp_brightness = int((kwargs.get(ATTR_BRIGHTNESS, 255) / 255) * 100)
+        greenwave.set_brightness(self._host, self._did, temp_brightness, self.token)
+        greenwave.turn_on(self._host, self._did, self.token)
 
     def turn_off(self, **kwargs):
         """Instruct the light to turn off."""
-        gateway_action(self._host, self._did, 'off', 0)
+        import greenwavereality as greenwave
+        greenwave.turn_off(self._host, self._did, self.token)
 
     def update(self):
         """Fetch new state data for this light."""
-        doc = pull_xml(self._host)
+        import greenwavereality as greenwave
+        doc = greenwave.grab_xml(self._host, self.token)
 
-        for device in \
-            doc['gwrcmds']['gwrcmd']['gdata']['gip']['room']['device']:
+        for device in doc:
             if device['did'] == self._did:
                 self._state = int(device['state'])
-                self._brightness = get_brightness(device)
-                self._online = is_online(device)
+                self._brightness = greenwave.hass_brightness(device)
+                self._online = greenwave.check_online(device)
                 self._name = device['name']
